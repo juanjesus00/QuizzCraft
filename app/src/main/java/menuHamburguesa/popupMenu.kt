6518ea02addb68bbb94494e9ecd95185135214fd
jjsmx24@gmail.com <jjsmx24@gmail.com>
2024-10-26 22:08:53 +0100

    navegador para login y register hecho, popup de opciones navegador en proceso

--- app/src/main/java/menuHamburguesa/popupMenu.kt
+++ app/src/main/java/menuHamburguesa/popupMenu.kt
@@ -9,19 +9,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import routes.NavigationActions
 import com.example.myapplication.R
+import uiPrincipal.poppinsFamily
 
 @Composable
 fun CustomPopupMenu(
@@ -40,11 +46,16 @@ fun CustomPopupMenu(
             ) {
                 Box(
                     modifier = Modifier
-                        .background(Color(color), shape = RoundedCornerShape(16.dp)) // Personaliza el fondo y las esquinas redondeadas
+                        .background(
+                            Color(color),
+                            shape = RoundedCornerShape(16.dp)
+                        ) // Personaliza el fondo y las esquinas redondeadas
                         .size(size.dp)
                 ) {
                     Column (
-                        modifier = Modifier.fillMaxSize().padding(start = 10.dp),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 10.dp),
                         verticalArrangement = Arrangement.SpaceAround,
                         horizontalAlignment = Alignment.Start
                     ){
@@ -61,17 +72,78 @@ fun CustomPopupMenu(
             ) {
                 Box(
                     modifier = Modifier
-                        .background(Color(color), shape = RoundedCornerShape(16.dp)) // Personaliza el fondo y las esquinas redondeadas
+                        .background(
+                            Color(color),
+                            shape = RoundedCornerShape(16.dp)
+                        ) // Personaliza el fondo y las esquinas redondeadas
                         .size(size.dp)
                 ) {
                     Column (
-                        modifier = Modifier.fillMaxSize().padding(start = 10.dp),
+                        modifier = Modifier
+                            .fillMaxSize(),
                         verticalArrangement = Arrangement.SpaceAround,
-                        horizontalAlignment = Alignment.Start
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ){
-                        popupInformation(icon = R.drawable.solar_user_bold, text = "¿Qué quieres hacer?",navigationActions)
-                        popupInformation(icon = R.drawable.ph_sign_in_bold, text = "Elección",navigationActions)
-                        popupInformation(icon = R.drawable.ligth_mode, text = "Interactivo",navigationActions)
+                        Text(
+                            text = "¿Qué quieres hacer?",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = poppinsFamily,
+                            color = Color(0xFF212325)
+                        )
+
+                        Row (
+                            modifier = Modifier
+                                .background(Color(0xFF212325), shape = RoundedCornerShape(20.dp))
+                                .size(height = 70.dp, width = 200.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Elección",
+                                modifier = Modifier
+                                /*.clickable {
+                                    when(text){
+                                        "Iniciar sesión" -> navigationActions.navigateToLogin()
+                                        "Registrarse" -> navigationActions.navigateToRegister()
+                                        else -> print("opcion no valida")
+                                    }
+                                }*/,
+                                color = Color(0xFFB18F4F),
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = poppinsFamily,
+                                fontSize = 15.sp
+                            )
+                        }
+
+                        Row (
+                            modifier = Modifier
+                                .background(Color(0xFF212325),shape = RoundedCornerShape(20.dp))
+                                .size(height = 70.dp, width = 200.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+
+                        ) {
+                            Text(
+                                text = "Interacción",
+                                modifier = Modifier
+                                    /*.clickable {
+                                        when(text){
+                                            "Iniciar sesión" -> navigationActions.navigateToLogin()
+                                            "Registrarse" -> navigationActions.navigateToRegister()
+                                            else -> print("opcion no valida")
+                                        }
+                                    }*/,
+                                color = Color(0xFFB18F4F),
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = poppinsFamily,
+                                fontSize = 15.sp
+                            )
+                        }
+
+
+
+
                     }
                 }
             }
