c2c983cb9955c1aa6f61fe802f08803e7adaeab8
juanjesus00 <jjsmx24@gmail.com>
2024-10-29 19:37:28 +0000

    uiQuizcraft introduccion de imagen de cuestionario y de documentos de texto

--- app/src/main/java/quizcraft/quizcraft.kt
+++ app/src/main/java/quizcraft/quizcraft.kt
@@ -14,13 +14,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -32,16 +36,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import com.example.myapplication.R
 import routes.NavigationActions
-import uiPrincipal.poppinsFamily
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun uiQuizCraft(navigationActions: NavigationActions, scrollState: ScrollState) {
 
@@ -51,19 +56,21 @@ fun uiQuizCraft(navigationActions: NavigationActions, scrollState: ScrollState) 
             .fillMaxSize()
             .background(Color(0xFFE0D4C8))
             .verticalScroll(scrollState)
-            .offset(x = 0.dp, y = 150.dp),
+            .padding(top = 150.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         
     ) {
-        InsertTexField(text = text, inputLabel = "Título del cuestionario")
-        FileUploader()
-        InsertTexField(text = text, inputLabel = "#Tags")
-        InsertTexField(text = text, inputLabel = "Descripción")
+        InsertTexField(text = text, inputLabel = "Título del cuestionario", size = 56)
+        FileUploader(image = R.drawable.camara, size = 150, typeFile = "image/*")
+        FileUploader(image = R.drawable.multiple_choice, size = 100, typeFile = "application/*")
+        FileUploader(image = R.drawable.document, size = 100, typeFile = "application/*")
+        InsertTexField(text = text, inputLabel = "#Tags", size = 56)
+        InsertTexField(text = text, inputLabel = "Descripción", size = 150)
     }
 
 }
 @Composable
-fun InsertTexField(text: String, inputLabel: String){
+fun InsertTexField(text: String, inputLabel: String, size: Int){
     var text by remember { mutableStateOf(text) }
     Box (
         modifier = Modifier
@@ -75,11 +82,13 @@ fun InsertTexField(text: String, inputLabel: String){
             label = { Text(text = inputLabel) },
             modifier = Modifier
                 .background(color = Color(0xFFFFFDFD), shape = RoundedCornerShape(20.dp))
-                .clip(RoundedCornerShape(20.dp)),
+                .clip(RoundedCornerShape(20.dp))
+                .height(size.dp),
             colors = TextFieldDefaults.colors(
                 focusedIndicatorColor = Color.Transparent,
                 unfocusedLabelColor = Color(0x80C49450),
-                unfocusedIndicatorColor = Color.Transparent
+                unfocusedIndicatorColor = Color.Transparent,
+
             )
         )
     }
@@ -87,7 +96,7 @@ fun InsertTexField(text: String, inputLabel: String){
 }
 
 @Composable
-fun FileUploader() {
+fun FileUploader(image: Int, size: Int, typeFile: String) {
     var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
     val context = LocalContext.current
 
@@ -97,21 +106,27 @@ fun FileUploader() {
     }
 
     Row(
-        modifier = Modifier
-            .padding(16.dp),
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
     ) {
-        Button(onClick = { launcher.launch("image/*") }) { // Set "image/*" to "application/*" for documents
-            Text("Select Image");
-            selectedImageUri?.let { uri ->
-                Image(
-                    painter = rememberAsyncImagePainter(uri),
-                    contentDescription = "Selected Image",
-                    modifier = Modifier
-                        .size(200.dp)
-                        .padding(16.dp)
-                )
+        Button(
+            onClick = { launcher.launch(typeFile) }, //solo se seleccion una imagen
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier.size(size.dp).border(2.dp, Color(0xFFC49450), RoundedCornerShape(20.dp)),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFEADEE6),
+            )
+        ) {
+            Box{
+                Image(painter = painterResource(id = image), contentDescription = "imagen de agregacion", modifier = Modifier.alpha(if(selectedImageUri == null) 1f else 0f))
+                selectedImageUri?.let { uri ->
+                    Image(
+                        painter = rememberAsyncImagePainter(uri),
+                        contentDescription = "Selected Image",
+                        modifier = Modifier.clip(RoundedCornerShape(20.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                }
             }
         }
 
