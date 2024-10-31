2d2eedbc6b9399d1bd7901f0b5a1604e2c27f6e1
juanjesus00 <jjsmx24@gmail.com>
2024-10-31 13:46:24 +0000

    uiQuizcraft introduccion de imagen de cuestionario y de documentos de texto

--- app/src/main/java/quizcraft/quizcraft.kt
+++ app/src/main/java/quizcraft/quizcraft.kt
@@ -42,10 +42,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.myapplication.R
 import routes.NavigationActions
+import uiPrincipal.poppinsFamily
 
 @Composable
 fun uiQuizCraft(navigationActions: NavigationActions, scrollState: ScrollState) {
@@ -62,13 +64,62 @@ fun uiQuizCraft(navigationActions: NavigationActions, scrollState: ScrollState) 
     ) {
         InsertTexField(text = text, inputLabel = "Título del cuestionario", size = 56)
         FileUploader(image = R.drawable.camara, size = 150, typeFile = "image/*")
-        FileUploader(image = R.drawable.multiple_choice, size = 100, typeFile = "application/*")
-        FileUploader(image = R.drawable.document, size = 100, typeFile = "application/*")
+        FileUploader2(image = R.drawable.multiple_choice, size = 100, typeFile = "application/*", text = "Generar con documento")
+        FileUploader2(image = R.drawable.document, size = 100, typeFile = "application/*", text = "Generar con texto")
         InsertTexField(text = text, inputLabel = "#Tags", size = 56)
         InsertTexField(text = text, inputLabel = "Descripción", size = 150)
     }
 
 }
+
+@Composable
+fun FileUploader2(image: Int, size: Int, typeFile: String, text: String) {
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+    val context = LocalContext.current
+
+    // Activity launcher for file selection
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        selectedImageUri = uri
+    }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Button(
+            onClick = { launcher.launch(typeFile) }, //solo se seleccion una imagen
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier.width(282.dp).height(56.dp).border(2.dp, Color(0xFFC49450), RoundedCornerShape(20.dp)),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFEADEE6),
+            )
+        ) {
+            Row (
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Text(text = text, fontFamily = poppinsFamily, color = Color(0xFFC49450))
+                Box{
+                    Image(painter = painterResource(id = image), contentDescription = "imagen de agregacion", modifier = Modifier.alpha(if(selectedImageUri == null) 1f else 0f))
+                    selectedImageUri?.let { uri ->
+                        Image(
+                            painter = rememberAsyncImagePainter(uri),
+                            contentDescription = "Selected Image",
+                            modifier = Modifier.clip(RoundedCornerShape(20.dp)),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+            }
+
+        }
+
+    }
+    Spacer(modifier = Modifier.height(50.dp))
+
+}
+
 @Composable
 fun InsertTexField(text: String, inputLabel: String, size: Int){
     var text by remember { mutableStateOf(text) }
